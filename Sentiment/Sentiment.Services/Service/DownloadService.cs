c822b589f97823575e3f0e93f82d95bb29cb1e6b
khayrulislam <bsse0822@iit.du.ac.bd>
2019-12-12 13:08:19 +0600

    downoad service create

---
+++ Sentiment/Sentiment.Services/Service/DownloadService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentiment.Services.Service
+{
+    public class DownloadService
+    {
+
+        public DownloadService()
+        {
+
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+
+
+    }
+}
