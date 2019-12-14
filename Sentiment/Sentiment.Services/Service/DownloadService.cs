562e888812181c5ef773fb18bc54009f3ed20295
khayrulislam <bsse0822@iit.du.ac.bd>
2019-12-14 10:08:16 +0600

    download xl file demo

--- Sentiment/Sentiment.Services/Service/DownloadService.cs
+++ Sentiment/Sentiment.Services/Service/DownloadService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +18,97 @@ namespace Sentiment.Services.Service
 
         }
 
-        public void Initialize()
+        private void Initialize()
         {
 
         }
 
+        private string GetFilePath()
+        {
+            var buildDir = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+            var path = Path.Combine(buildDir, "bin\\download.xlsx");
 
+            var newPath = Path.Combine(buildDir, Guid.NewGuid().ToString() + ".xlsx");
+
+            File.Copy(path, newPath);
+
+
+            return newPath;
+        }
+
+
+        public byte[] GetRepositoryContent()
+        {
+            var outputPath = GetFilePath();
+
+            using (SpreadsheetDocument myDoc = SpreadsheetDocument.Open(outputPath, true))
+            {
+                WorkbookPart workbookPart = myDoc.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                string origninalSheetId = workbookPart.GetIdOfPart(worksheetPart);
+
+                WorksheetPart replacementPart = workbookPart.AddNewPart<WorksheetPart>();
+                string replacementPartId = workbookPart.GetIdOfPart(replacementPart);
+
+                OpenXmlReader reader = OpenXmlReader.Create(worksheetPart);
+                OpenXmlWriter writer = OpenXmlWriter.Create(replacementPart);
+
+
+                Row r = new Row();
+                Cell c = new Cell();
+                CellFormula f = new CellFormula();
+                f.CalculateCell = true;
+                f.Text = "RAND()";
+                c.Append(f);
+                CellValue v = new CellValue();
+                c.Append(v);
+
+                while (reader.Read())
+                {
+                    if (reader.ElementType == typeof(SheetData))
+                    {
+                        if (reader.IsEndElement)
+                            continue;
+                        writer.WriteStartElement(new SheetData());
+
+                        for (int row = 0; row < 10; row++)
+                        {
+                            writer.WriteStartElement(r);
+                            for (int col = 0; col < 10; col++)
+                            {
+                                writer.WriteElement(c);
+                            }
+                            writer.WriteEndElement();
+                        }
+
+                        writer.WriteEndElement();
+                    }
+                    else
+                    {
+                        if (reader.IsStartElement)
+                        {
+                            writer.WriteStartElement(reader);
+                        }
+                        else if (reader.IsEndElement)
+                        {
+                            writer.WriteEndElement();
+                        }
+                    }
+                }
+
+                reader.Close();
+                writer.Close();
+
+                Sheet sheet = workbookPart.Workbook.Descendants<Sheet>()
+                .Where(s => s.Id.Value.Equals(origninalSheetId)).First();
+                sheet.Id.Value = replacementPartId;
+                workbookPart.DeletePart(worksheetPart);
+
+
+            }
+            return File.ReadAllBytes(outputPath);
+
+        }
 
     }
 }
---
+++ Sentiment/Sentiment.WebAPI/Controllers/DownloadController.cs
@@ -0,0 +1,35 @@
+﻿using Sentiment.Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Sentiment.WebAPI.Controllers
+{
+    public class DownloadController : ApiController
+    {
+        private DownloadService downloadService = new DownloadService();
+
+        [HttpGet]
+        public HttpResponseMessage DownloadRepository()
+        {
+            var data = downloadService.GetRepositoryContent();
+
+
+            var response = new HttpResponseMessage();
+
+            response.StatusCode = HttpStatusCode.OK;
+            response.Content = new ByteArrayContent(data) ;
+            response.Content.Headers.ContentLength = data.Length;
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = "download" + ".xlsx";
+
+            return response;
+        }
+
+    }
+}
